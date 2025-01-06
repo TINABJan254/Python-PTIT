c2d5620d8887c270f551a1a619c501183016f3fd
Tinab <trandinhthien0104@gmail.com>
2025-01-06 21:06:06 +0700

    .

--- Learning/hello.py
+++ Learning/hello.py
@@ -1,13 +1,20 @@
-class Person:
-    nationality = "Viet Nam"
-    def __init__(self, name, age):
-        self.name = name
-        self.__age = age
+import json
 
-    def getAge(self):
-        return self.__age
+
+# main
+t = int(input())
+for _ in range(t):
+    line = input()
+    a = json.loads(line)
+
+    l = []
+    s = 0
+    cnt = 0
+    for x in a:
+        if str(a[x]).isnumeric():
+            l.append(x)
+            s += a[x]
+        if str(a[x]).isalpha():
+            cnt += 1
     
-p1 = Person("Thien", 23)
-p2 = Person("Tran", 32)
-
-print(p1.getAge())
+    print(f'({l}, {s}, {cnt})')
